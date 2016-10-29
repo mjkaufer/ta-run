920b73fb88351398460fb330661b57ca8f1536eb
pwstegman <pwstegman@gmail.com>
2016-10-28 20:57:23 -0500

    Cleaned code
    If you aren't going to use semicolons, then go all in

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 var player = require('play-sound')(opts = {})
 
 // play with a callback
-var names = ['ta','aa','ru','uu','un'];
+var names = ['ta','aa','ru','uu','un']
 
-var maxIndex = names.length;
+var maxIndex = names.length
 
 
 function playSound(i){
 	if(i > names.length)
-		return;
+		return
 
 	player.play('./noises/' + names[i] + '.wav', function(){
 		playSound(i+1)
